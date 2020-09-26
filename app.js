f42f2302dfc26ea798ee5fe9a6871a1ace02b721
Kunal Sharma <kunal.sharma@firstinsight.com>
2020-09-27 01:54:48 +0530

    add a frontend-build script

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+
+// Handles any requests that don't match the ones above and redirects them to
+// the frontend
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 module.exports = app;
