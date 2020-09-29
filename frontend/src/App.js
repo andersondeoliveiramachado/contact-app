46bc57043b8269961dcbd1746c75cfd477fd07c6
Kunal Sharma <kunal.sharma@firstinsight.com>
2020-09-29 13:39:23 +0530

    create ContactCard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {createMuiTheme} from "@material-ui/core/styles";
+import {ThemeProvider} from "@material-ui/styles";
+import SignUp from "./SignUp/SignUp";
+import Home from "./Home/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#0A45C2'
+        }
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <ThemeProvider theme={theme}>
+                <Switch>
+                    <Route exact path="/signin" component={SignUp}/>
+                    <PrivateRoute path="/">
+                        <Home/>
+                    </PrivateRoute>
+                </Switch>
+            </ThemeProvider>
+        </Router>
+    );
 }
 
 export default App;
