d1c728b457fd5b846cef6670d1cbb287c9b63688
Kunal Sharma <kunal.sharma@firstinsight.com>
2020-09-29 13:33:07 +0530

    finish up backend changes

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var userRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
+
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
+app.use('/user', userRouter);
 app.use('/auth', authRouter);
 
 // Handles any requests that don't match the ones above and redirects them to
